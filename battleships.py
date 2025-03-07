3b65ec96e9bd396068716f7148a2606e2f72adfd
RasmusKB <rasmusbkp@gmail.com>
2025-03-07 12:32:07 +0100

    Working battleships api

--- battleships.py
+++ battleships.py
@@ -21,6 +21,7 @@ class GuessReturn(Enum):
     hit = "hit"
     miss = "miss"
     dupe_guess = "dupe_guess"
+    out_of_bounds = "out_of_bounds"
     finished_game = "finished_game"
 
 class Ship:
@@ -33,13 +34,13 @@ class Ship:
             raise ValueError("Invalid ship configuration")
 
         if startx - endx > 0:
-            self.filled = {(startx + i, starty) for i in range((startx - endx) + 1)}
+            self.filled = {(startx - i, starty) for i in range((startx - endx) + 1)}
         elif startx - endx < 0:
-            self.filled = {(startx - i, starty) for i in range((endx - startx) + 1)}
+            self.filled = {(startx + i, starty) for i in range((endx - startx) + 1)}
         elif starty - endy > 0:
-            self.filled = {(startx, starty + i) for i in range((starty - endy) + 1)}
+            self.filled = {(startx, starty - i) for i in range((starty - endy) + 1)}
         elif starty - endy < 0:
-            self.filled = {(startx, starty - i) for i in range((endy - starty) + 1)}
+            self.filled = {(startx, starty + i) for i in range((endy - starty) + 1)}
         else:
             raise ValueError("Invalid ship configuration")
 
@@ -48,38 +49,62 @@ class Ship:
 class Game:
     def __init__(self, board_size: tuple[int, int], ships: list[Ship]) -> None:
         self.width, self.height = board_size
-        self.p1_board = PlayerBoard((0,self.width//2-1),(0,self.y ),[ship for ship in ships if ship.player == 1])
-        self.p2_board = PlayerBoard((0,self.width//2),(0,self.y),[ship for ship in ships if ship.player == 2])
-        self.current_board = p2_board
+        self.p1_board = PlayerBoard((0,self.width//2-1),(0,self.height-1),[ship for ship in ships if ship.player == 1], 1)
+        self.p2_board = PlayerBoard((self.width//2, self.width-1),(0,self.height-1),[ship for ship in ships if ship.player == 2], 2)
+        self.alternate = self.alternator()
+        self.switch_turn()
+
+    def alternator(self):
+        while True:
+            yield self.p2_board
+            yield self.p1_board
+
+    def switch_turn(self):
+        self.current_board = next(self.alternate)
+
+    def current_player(self):
+        match self.current_board.player_num:
+            case 1:
+                return 2
+            case 2:
+                return 1
 
     def make_guess(self, guess: tuple[int, int]) -> GuessReturn:
-        current_board.make_guess(guess)
-
-
-
-
-
-
+        game_state = self.current_board.make_guess(guess)
+        match game_state:
+            case GuessReturn.out_of_bounds:
+                print(f"The guess was {guess}")
+                return game_state
+            case GuessReturn.dupe_guess:
+                return game_state
+            case GuessReturn.finished_game:
+                return game_state
+            case _:
+                self.switch_turn()
+                return game_state
 
 class PlayerBoard:
-    def __init__(self, width: tuple[int, int], height: tuple[int, int], ships: list[Ship]) -> None:
+    def __init__(self, width: tuple[int, int], height: tuple[int, int], ships: list[Ship], player_num: int) -> None:
         """
         ships is given as a dict with the beginning position of the ship as key, and size as value
         """
-        self.x = 12
-        self.y = 7
+        self.x = width
+        self.y = height
         self.player_num = player_num
-        self.ships = []
+        self.ships = ships
         self.dead_ships = []
         self.guesses: set[tuple[int, int]] = set()
         self.board: dict[tuple[int, int], Ship] = {}
+        self.add_ships(ships)
 
     def add_ships(self, ships: list[Ship]):
         for ship in ships:
-            self.ships.append(ship)
-
             for coord in ship.filled:
                 self.board[coord] = ship  # will make references
+
+    def in_bounds(self, coord: tuple[int, int]) -> bool:
+        return (self.x[0] <= coord[0] <= self.x[1]
+            and self.y[0] <= coord[1] <= self.y[1])
 
     def make_guess(self, coord: tuple[int, int]) -> GuessReturn:
         """
@@ -87,40 +112,45 @@ class PlayerBoard:
 
         The coord param should be in the coordinate system of the full air table (i.e. 12x14)
         """
-        calibrated_coord = coord
-        if self.player_num == 2:
-            x, y = coord
-            calibrated_coord = (x, y + self.y)
-
         if coord in self.guesses:
             return GuessReturn.dupe_guess
 
+        if not self.in_bounds(coord):
+            return GuessReturn.out_of_bounds
+
         self.guesses.add(coord)
 
-        if self.board[coord] is not None:
+        if tableActive:
+            t.burst(coord)
+        else:
+            print(coord)
+
+        if self.board.get(coord) is not None:
             self.board[coord].lives -= 1
             if self.board[coord].lives == 0:
                 self.dead_ships.append(self.board[coord])
                 print("A ship has been sunk")
 
-        if tableActive:
-            t.burst(calibrated_coord)
-        else:
-            print(calibrated_coord)
-
         if len(self.dead_ships) == len(self.ships):
             return GuessReturn.finished_game
 
-        if self.board[coord] is not None:
+        if self.board.get(coord) is not None:
             return GuessReturn.hit
 
         return GuessReturn.miss
 
 
 def main():
-    p1_board = PlayerBoard(1)
-    p2_board = PlayerBoard(2)
+    if tableActive:
+        t.clear()
 
+    game = Game((14, 12), [Ship((0,0),(4,0),1), Ship((13,12),(13,10),2)])
+    while True:
+        result = game.make_guess(eval(input(f"Player {game.current_player()} make a guess\n")))
+        print(result)
+        if result == GuessReturn.finished_game:
+            print(f"PLAYER {game.current_player()} WON")
+            return
 
 if __name__ == "__main__":
     main()
