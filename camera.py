b4a15fd1f55215ad3f24e6ed4c7f02b55024fe5b
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-03-07 15:59:56 +0100

    filters out some

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+from sklearn.neighbors import KDTree
+from sklearn.neighbors import NearestNeighbors
 
 
 class Camera:
@@ -18,11 +19,44 @@ class Camera:
         grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         cv2.imwrite("DEBUG-grey.png", grey)
 
-        _, thresh = cv2.threshold(grey, 254, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+        gauss = cv2.GaussianBlur(grey,(5,5),0)
+        cv2.imwrite("DEBUG-gauss.png", gauss)
+
+        _, thresh = cv2.threshold(gauss, 200, 255, cv2.THRESH_BINARY)
         cv2.imwrite("DEBUG-thresh.png", thresh)
         cnts, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cimg = cv2.drawContours(image, cnts,-1, (120,20,20),3)
-        cv2.imwrite("result.png", cimg)
+        cv2.imwrite("DEBUG-cont.png", cimg)
+
+        centers = []
+
+        for c in cnts:
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"]) if M["m00"] != 0 else 0
+            cY = int(M["m01"] / M["m00"]) if M["m00"] != 0 else 0
+            
+            centers.append((cX,cY))
+
+        tree = KDTree(centers)
+
+        refined_centers = set()
+
+        for center in centers:
+            for idx_arr in tree.query_radius([center], r=20):
+                if len(idx_arr) > 3:
+                    for idx in idx_arr:
+                        refined_centers.add(centers[idx])
+
+        for center in refined_centers:
+            image = cv2.circle(image, center, 5, (20,120,20))
+
+        cv2.imshow("lol",image)
+        cv2.waitKey(0)
+
+
+        
+
+
 
 
 cam = Camera()
