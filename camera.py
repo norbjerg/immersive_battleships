e6e262ceafe5d5fb5ec0e47da586054825463b57
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-03-07 14:57:00 +0100

    detects

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class Camera:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
@@ -10,18 +11,18 @@ class Camera:
         result, image = self.cam.read()
         if result:
             return image
-        raise RunTimeError("Could not capture image")
+        raise RuntimeError("Could not capture image")
 
     def otsu_thresh(self):
         image = cv2.imread("Board.png")
         grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         cv2.imwrite("DEBUG-grey.png", grey)
 
-        _, thresh = cv2.threshold(grey, 180, 255, cv2.THRESH_BINARY)
+        _, thresh = cv2.threshold(grey, 254, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
         cv2.imwrite("DEBUG-thresh.png", thresh)
-
-    def k_mean(self):
-
+        cnts, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cimg = cv2.drawContours(image, cnts,-1, (120,20,20),3)
+        cv2.imwrite("result.png", cimg)
 
 
 cam = Camera()
