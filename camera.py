201b9798f3ea4d4176c13d80f0d2cf7cdadd6820
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-03-25 12:16:43 +0100

    almost works

--- camera.py
+++ camera.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
+from cv2 import aruco
 from sklearn.neighbors import KDTree
-from sklearn.neighbors import NearestNeighbors
+
+BOT_LEFT_ARUCO_ID = 0
+TOP_RIGHT_ARUCO_ID = 1
 
 
 class Camera:
@@ -15,18 +18,27 @@ class Camera:
         raise RuntimeError("Could not capture image")
 
     def otsu_thresh(self):
-        image = cv2.imread("Board.png")
+        image = cv2.imread("Board_w_aruco.png")
+
+        aruco_corners, ids, rejectedImgPoints = aruco.detectMarkers(
+            image, aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
+        )
+
+        ids = ids.reshape((ids.shape[0],))
+        # four corners returned in their original order (which is clockwise starting with top left)
+        ids_to_corners = dict(zip(ids, aruco_corners))
+
         grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         cv2.imwrite("DEBUG-grey.png", grey)
 
-        gauss = cv2.GaussianBlur(grey,(5,5),0)
+        gauss = cv2.GaussianBlur(grey, (5, 5), 0)
         cv2.imwrite("DEBUG-gauss.png", gauss)
 
         _, thresh = cv2.threshold(gauss, 200, 255, cv2.THRESH_BINARY)
         cv2.imwrite("DEBUG-thresh.png", thresh)
         cnts, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        cimg = cv2.drawContours(image, cnts,-1, (120,20,20),3)
-        cv2.imwrite("DEBUG-cont.png", cimg)
+        # cimg = cv2.drawContours(image, cnts,-1, (120,20,20),3)
+        # cv2.imwrite("DEBUG-cont.png", cimg)
 
         centers = []
 
@@ -34,29 +46,20 @@ class Camera:
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"]) if M["m00"] != 0 else 0
             cY = int(M["m01"] / M["m00"]) if M["m00"] != 0 else 0
-            
-            centers.append((cX,cY))
-
-        tree = KDTree(centers)
-
-        refined_centers = set()
+            if (
+                cX > ids_to_corners[BOT_LEFT_ARUCO_ID][0][1][0]  # top right corner x
+                and cY < ids_to_corners[BOT_LEFT_ARUCO_ID][0][1][1]  # top right corner y
+                and cX < ids_to_corners[TOP_RIGHT_ARUCO_ID][0][3][0]  # bottom left corner x
+                and cY > ids_to_corners[TOP_RIGHT_ARUCO_ID][0][3][1] + 2  # bottom left corner y
+            ):
+                centers.append((cX, cY))
 
         for center in centers:
-            for idx_arr in tree.query_radius([center], r=20):
-                if len(idx_arr) > 3:
-                    for idx in idx_arr:
-                        refined_centers.add(centers[idx])
+            cv2.circle(image, center, 5, (20, 120, 20))
 
-        for center in refined_centers:
-            image = cv2.circle(image, center, 5, (20,120,20))
-
-        cv2.imshow("lol",image)
-        cv2.waitKey(0)
-
+        cv2.imwrite("result.png", image)
 
         
-
-
 
 
 cam = Camera()
