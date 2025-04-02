8d8c0eb924602312300ec7bd20732bb7f5e32383
RasmusKB <rasmusbkp@gmail.com>
2025-04-02 18:20:31 +0200

    Finished unit tests for the battleship game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Tests:
-To run tests use python3 -m tests.battleship_tests
+Testing:
+The tests are located in the "tests" directory. These can be run from the root of the project using:
+```
+python3 -m tests.battleship_tests -b
+```
--- tests/battleship_tests.py
+++ tests/battleship_tests.py
@@ -1,14 +1,83 @@
 import unittest
 from battleships import *
 
-#class TestGame(unittest.TestCase):
+class TestGame(unittest.TestCase):
+    def test_switch_turn(self):
+        game = Game((10,10),[])
+        self.assertEqual(game.current_board.player_num, 2)
+        game.switch_turn()
+        self.assertEqual(game.current_board.player_num, 1)
+
+    def test_current_turn(self):
+        game = Game((10,10),[])
+        self.assertEqual(game.current_board.player_num, 2)
+        self.assertEqual(game.current_player(), 1)
+        game.switch_turn()
+        self.assertEqual(game.current_board.player_num, 1)
+        self.assertEqual(game.current_player(), 2)
+
+    def test_make_guess(self):
+        game = Game((10,10),[Ship((8, 9), (9, 9), 2)])
+        self.assertEqual(game.make_guess((1,1)), GuessReturn.out_of_bounds)
+        self.assertEqual(game.current_player(), 1)
+        self.assertEqual(game.make_guess((8, 8)), GuessReturn.miss)
+        self.assertEqual(game.current_player(), 2)
+        game.switch_turn()
+        self.assertEqual(game.make_guess((8, 9)), GuessReturn.hit)
+        self.assertEqual(game.current_player(), 2)
+        game.switch_turn()
+        self.assertEqual(game.make_guess((8, 9)), GuessReturn.dupe_guess)
+        self.assertEqual(game.current_player(), 1)
+        self.assertEqual(game.make_guess((9, 9)), GuessReturn.finished_game)
+
+
+
+
+    def make_guess(self, guess: tuple[int, int]) -> GuessReturn:
+        game_state = self.current_board.make_guess(guess)
+        match game_state:
+            case GuessReturn.out_of_bounds:
+                print(f"The guess was {guess}")
+                return game_state
+            case GuessReturn.dupe_guess:
+                return game_state
+            case GuessReturn.finished_game:
+                return game_state
+            case _:
+                self.switch_turn()
+                return game_state
 
 class TestPlayerBoard(unittest.TestCase):
-    def test_add_ships(self):
-        self.p1_board = PlayerBoard((0,self.width//2-1),(0,self.height-1),[ship for ship in ships if ship.player == 1], 1)
-        test_player = PlayerBoard((0,10),(0,10),[Ship((0,0),(0,2), 1)]
-        self.assertTrue(
+    def test_add_ship(self):
+        board = PlayerBoard((0, 10), (0, 10), [Ship((0, 0), (0, 2), 1)], 1)
+        expected_coords = [(0, 0), (0, 1), (0, 2)]
+        for coord in expected_coords:
+            self.assertIn(coord, board.board, f"Coordinate {coord} not found in board after initialization")
 
+        with self.assertRaises(ValueError) as context:
+            board.add_ships([Ship((0, 0), (0, 11), 1)])
+
+        self.assertIn("Ship is out of bounds", str(context.exception))
+
+    def test_in_bounds(self):
+        board = PlayerBoard((0, 10), (0, 10), [Ship((0, 0), (0, 2), 1)], 1)
+        self.assertTrue(board.in_bounds((5,5)))
+        self.assertTrue(board.in_bounds((10,0)))
+        self.assertTrue(board.in_bounds((0,10)))
+        self.assertTrue(board.in_bounds((10,10)))
+
+        self.assertFalse(board.in_bounds((11,10)))
+        self.assertFalse(board.in_bounds((10,11)))
+        self.assertFalse(board.in_bounds((-1,0)))
+        self.assertFalse(board.in_bounds((0,-1)))
+
+    def test_make_guess(self):
+        board = PlayerBoard((0, 10), (0, 10), [Ship((0, 0), (0, 1), 1)], 1)
+        self.assertEqual(board.make_guess((0,1)), GuessReturn.hit)
+        self.assertEqual(board.make_guess((0,1)), GuessReturn.dupe_guess)
+        self.assertEqual(board.make_guess((-1,0)), GuessReturn.out_of_bounds)
+        self.assertEqual(board.make_guess((0,10)), GuessReturn.miss)
+        self.assertEqual(board.make_guess((0,0)), GuessReturn.finished_game)
 
 class TestShip(unittest.TestCase):
     def test_vertical_ship(self):
