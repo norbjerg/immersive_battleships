14602d6df470c1f1bf7341c5173ea3d5b17db217
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-04-04 14:13:00 +0200

    hit and miss

--- ui.py
+++ ui.py
@@ -1,61 +1,142 @@
+from random import randint
+import time
 import pyglet
 from pyglet import shapes, text
 
 
-class Interface:
-    def __init__(self) -> None:
-        self.window = pyglet.window.Window()
-
-    def main(self):
-        width = 140 * 5
-        height = 120 * 5
+class Interface(pyglet.window.Window):
+    def __init__(self):
+        super().__init__()
+        self.set_size(1500, 600)
+        x_size = 12
+        y_size = 7
+        width = x_size * 50
+        height = y_size * 55
+        self.key_handler = pyglet.window.key.KeyStateHandler()
+        self.push_handlers(self.key_handler)
         board_corner_x, board_corner_y = (
-            self.window.width - width - 20,
-            self.window.height - height - 20,
+            self.width - width - 20,
+            self.height - height - 20,
         )
-        board = shapes.BorderedRectangle(board_corner_x, board_corner_y, width, height)
-        xnumbers = [
+        self.board = shapes.BorderedRectangle(board_corner_x, board_corner_y, width, height)
+        self.xnumbers = [
             text.Label(
                 str(x),
-                board_corner_x + x * 45 + 40,
+                board_corner_x + idx * 45 + 40,
                 board_corner_y + 20,
                 color=(0, 0, 0, 255),
             )
-            for x in range(14)
+            for idx, x in enumerate("ABCDEFGHIJKL")
         ]
-        ynumbers = [
+        self.xnumbers.append(
+            text.Label(
+                "x",
+                board_corner_x + x_size * 45 + 40,
+                board_corner_y + 20,
+                color=(0, 0, 0, 255),
+            )
+        )
+        self.ynumbers = [
             text.Label(
                 str(y),
                 board_corner_x + 20,
                 board_corner_y + y * 45 + 60,
                 color=(0, 0, 0, 255),
             )
-            for y in range(12)
+            for y in range(y_size)
         ]
-        board_coordinate_to_dot = {
+        self.ynumbers.append(
+            text.Label(
+                "y",
+                board_corner_x + 20,
+                board_corner_y + y_size * 45 + 40,
+                color=(0, 0, 0, 255),
+            )
+
+        )
+        self.board_coordinate_to_dot = {
             (x, y): shapes.Circle(
-                board_corner_x + x * 45 + 40,
-                board_corner_y + y * 45 + 60,
+                board_corner_x + x * 45 + 40 + 5,
+                board_corner_y + y * 45 + 60 + 5,
                 2,
                 color=(0, 0, 0, 255),
             )
-            for x in range(14)
-            for y in range(12)
+            for x in range(x_size)
+            for y in range(y_size)
         }
+        self.misses: list[shapes.ShapeBase] = []
+        self.hits: list[text.DocumentLabel] = []
 
-        @self.window.event
-        def on_draw():
-            self.window.clear()
-            board.draw()
-            for num in xnumbers:
-                num.draw()
-            for num in ynumbers:
-                num.draw()
-            for c in board_coordinate_to_dot.values():
-                c.draw()
+    def next_frame(self):
+        self.dispatch_events()
+        self.clear()
+        self.board.draw()
+        for num in self.xnumbers:
+            num.draw()
+        for num in self.ynumbers:
+            num.draw()
+        for c in self.board_coordinate_to_dot.values():
+            c.draw()
+        for m in self.misses:
+            m.draw()
+        for h in self.hits:
+            h.draw()
+        self.flip()
 
-        pyglet.app.run()
+    def miss(self, coord: tuple[int,int]):
+        """
+        Draw miss on board.
+        
+        Note that coord is in coordinates the player gives as guess (12x7).
+        """
+        miss_node = self.board_coordinate_to_dot[coord]
+        self.misses.append(
+            shapes.Circle(
+                miss_node.x,
+                miss_node.y,
+                5,
+                color=(20,20,255,255)
+            )
+        )
+
+    def hit(self, coord: tuple[int,int]):
+        """
+        Draw hit on board.
+        
+        Note that coord is in coordinates the player gives as guess (12x7).
+        """
+        hit_node = self.board_coordinate_to_dot[coord]
+        self.hits.append(
+            text.Label(
+                "X",
+                hit_node.x-1,
+                hit_node.y+5,
+                anchor_x="center",
+                anchor_y="center",
+                font_size=20,
+                color=(255,20,20,255)
+            )
+        )
+
 
 
 i = Interface()
-i.main()
+last_time = time.perf_counter()
+filled = set()
+while True:
+    if i.key_handler[pyglet.window.key.ESCAPE]:
+        break
+    if i.key_handler[pyglet.window.key.M]:
+        guess = (randint(0,11),randint(0,6))
+        while guess in filled:
+            guess = (randint(0,11),randint(0,6))
+        i.miss(guess)
+    if i.key_handler[pyglet.window.key.H]:
+        guess = (randint(0,11),randint(0,6))
+        while guess in filled:
+            guess = (randint(0,11),randint(0,6))
+        i.hit(guess)
+    elapsed_time = time.perf_counter() - last_time
+    if elapsed_time > 1/60:
+        last_time = time.perf_counter()
+        i.next_frame()
