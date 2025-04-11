5afff3b8304e06cc3f007742ac6c11986403ddc4
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-04-11 10:53:07 +0200

    Create aruco_map.py

---
+++ aruco_map.py
@@ -0,0 +1,102 @@
+PLAYER1_GUESS_CONFIRM = 29
+PLAYER2_GUESS_CONFIRM = 49
+
+
+PLAYER1_VERTICAL_Y_COORD_TO_ARUCO_ID = {
+    10: 0,
+    11: 1,
+    12: 2,
+    13: 3,
+    14: 4,
+    15: 5,
+    16: 6,
+}
+PLAYER1_HORIZONTAL_X_COORD_TO_ARUCO_ID = {
+    17: 0,
+    18: 1,
+    19: 2,
+    20: 3,
+    21: 4,
+    22: 5,
+    23: 6,
+    24: 7,
+    25: 8,
+    26: 9,
+    27: 10,
+    28: 11,
+}
+
+PLAYER2_VERTICAL_NUMBER_TO_ARUCO_ID = {
+    30: 7,
+    31: 8,
+    32: 9,
+    33: 10,
+    34: 11,
+    35: 12,
+    36: 13,
+}
+PLAYER2_HORIZONTAL_LETTER_TO_ARUCO_ID = {
+    37: 0,
+    38: 1,
+    39: 2,
+    40: 3,
+    41: 4,
+    42: 5,
+    43: 6,
+    44: 7,
+    45: 8,
+    46: 9,
+    47: 10,
+    48: 11,
+}
+
+
+# NOTES
+PLAYER1_VERTICAL_NUMBER_TO_ARUCO_ID = {
+    0: 10,
+    1: 11,
+    2: 12,
+    3: 13,
+    4: 14,
+    5: 15,
+    6: 16,
+}
+PLAYER1_HORIZONTAL_LETTER_TO_ARUCO_ID = {
+    "A": 17,
+    "B": 18,
+    "C": 19,
+    "D": 20,
+    "E": 21,
+    "F": 22,
+    "G": 23,
+    "H": 24,
+    "I": 25,
+    "J": 26,
+    "K": 27,
+    "L": 28,
+}
+
+
+PLAYER2_VERTICAL_NUMBER_TO_ARUCO_ID = {
+    0: 30,
+    1: 31,
+    2: 32,
+    3: 33,
+    4: 34,
+    5: 35,
+    6: 36,
+}
+PLAYER2_HORIZONTAL_LETTER_TO_ARUCO_ID = {
+    "A": 37,
+    "B": 38,
+    "C": 39,
+    "D": 40,
+    "E": 41,
+    "F": 42,
+    "G": 43,
+    "H": 44,
+    "I": 45,
+    "J": 46,
+    "K": 47,
+    "L": 48,
+}
