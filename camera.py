25eb13e1b9d2d179431a126acdcad0d3057c3fbf
norbjerg <120656170+norbjerg@users.noreply.github.com>
2025-05-13 11:02:22 +0200

    update color masks

--- camera.py
+++ camera.py
@@ -99,29 +99,29 @@ class Camera:
         """
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_blue = np.array([100, 90, 50])
-        upper_blue = np.array([130, 255, 255])
+        lower_blue = np.array([100, 70, 50])
+        upper_blue = np.array([140, 255, 255])
         blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
         cnts_blue = imutils.grab_contours(
             cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         )
 
-        lower_green = np.array([35, 90, 50])
+        lower_green = np.array([35, 80, 50])
         upper_green = np.array([85, 255, 255])
         green_mask = cv2.inRange(hsv, lower_green, upper_green)
         cnts_green = imutils.grab_contours(
             cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         )
 
-        lower_magenta = np.array([135, 90, 50])
+        lower_magenta = np.array([145, 80, 50])
         upper_magenta = np.array([175, 255, 255])
         magenta_mask = cv2.inRange(hsv, lower_magenta, upper_magenta)
         cnts_magenta = imutils.grab_contours(
             cv2.findContours(magenta_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         )
 
-        lower_red1 = np.array([0, 220, 100])
+        lower_red1 = np.array([0, 180, 100])
         upper_red1 = np.array([10, 255, 255])
         red_mask = cv2.inRange(hsv, lower_red1, upper_red1)
 
@@ -137,7 +137,7 @@ class Camera:
         ):
             for cnt in contours:
                 area = cv2.contourArea(cnt)
-                if area > 50:
+                if area > 20 and area < 400:
                     M = cv2.moments(cnt)
                     cX = int(M["m10"] / M["m00"]) if M["m00"] != 0 else 0
                     cY = int(M["m01"] / M["m00"]) if M["m00"] != 0 else 0
